8806757e9237dd8893dd2a39025814230ea8b95e
Derin <derin.amal@tum.de>
2025-04-01 15:16:12 +0200

    add authorization token feature

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,17 @@ const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+	const token = req.headers['authorization'];
+
+	if (token !== 'Password123') {
+		return res.status(401).json({ error: 'Unauthorized' });
+	}
+
+	next();
+});
+
 app.use('/projects', projectsRoutes);
 app.use('/reports', reportsRoutes);
 
