71a0e3603cc73be4a37e1bdb9ea6143a645d86b5
Derin <derin.amal@tum.de>
2025-04-01 12:56:00 +0200

    add GET all projects endpoint

--- src/routes/projects.routes.ts
+++ src/routes/projects.routes.ts
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import db from '../services/db.service';
 
 const router = Router();
 
+//GET all projects
 router.get('/', (req, res) => {
-	res.json({ message: 'All projects go here' });
+	try {
+		const projects = db.query('SELECT * FROM projects');
+		res.json(projects);
+	} catch (err) {
+		res.status(500).json({ error: 'Something went wrong' });
+	}
 });
 
 export default router;
