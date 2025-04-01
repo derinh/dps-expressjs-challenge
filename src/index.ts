fc7047a6faa2c028e1aab528006c009aee19e99e
Derin <derin.amal@tum.de>
2025-04-01 12:20:25 +0200

    first change for testing

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,10 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.get('/projects', (req, res) => {
+	res.json({ message: 'Hello from /projects!' });
+});
+
 app.listen(port, () => {
 	console.log(`[server]: Server is running at http://localhost:${port}`);
 });
